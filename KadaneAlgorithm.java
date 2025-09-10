a5229142e7bc6c97867dff5aef16a6cd7230203d
Athul Raj <athulraj786@gmail.com>
2025-09-10 20:45:56 +0530

    kadane algorithm

---
+++ KadaneAlgorithm.java
@@ -0,0 +1,23 @@
+public class KadaneAlgorithm {
+    public static int maxSubArraySum(int[] arr) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, -8, 7, -1, 2, 3};
+        int[] arr2 = {-2, -4};
+        int[] arr3 = {5, 4, 1, 7, 8};
+
+        System.out.println("Max Subarray Sum: " + maxSubArraySum(arr1)); // 11
+        System.out.println("Max Subarray Sum: " + maxSubArraySum(arr2)); // -2
+        System.out.println("Max Subarray Sum: " + maxSubArraySum(arr3)); // 25
+    }
+}
---
+++ ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+    public static int[] reverse(int[] Arr, int start, int end){
+        while(start<end){
+            int temp = Arr[start];
+            Arr[start] = Arr[end];
+            Arr[end] = temp;
+            start++;end --;
+            
+        }
+        return Arr;
+    }
+    public static void printarray(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args){
+        int[] Arr = {2,4,5,6,7,8};
+        printarray(Arr);  
+        reverse(Arr, 0, Arr.length-1);
+        System.out.println("**********************");
+        printarray(Arr);
+    }
+}
