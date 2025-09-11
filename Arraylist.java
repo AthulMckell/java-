7d836b8defd782ee4ac36f22ee7b233b84b30754
Athul Raj <athulraj786@gmail.com>
2025-09-11 12:45:48 +0530

    matrixSearch

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Arraylist {
+    public static void main(String[] args){
+        ArrayList<String> Arr = new ArrayList<>(Arrays.asList("Athul","Amy"));
+        System.out.println(Arr);
+        System.out.println(Arr.get(0));
+        Arr.add("Amla");
+        System.out.println(Arr);
+    }
+}
