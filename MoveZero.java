1285172fbe04cf26daa4d44be1f9c8644aa4dd77
Athul Raj <athulraj786@gmail.com>
2025-09-17 01:48:36 +0530

    Move zero

---
+++ MoveZero.java
@@ -0,0 +1,30 @@
+public class MoveZero {
+
+    public static void printarray(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void move(int[] arr){
+        int j=0;
+        for(int i = 0;i<arr.length; i++)
+        {
+            if(arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            if(arr[j] != 0){
+                j++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr ={4,3,0,8,6,0,4};
+        printarray(arr);
+        move(arr);
+        printarray(arr);
+    }
+}
---
+++ SecMax.java
@@ -0,0 +1,22 @@
+public class SecMax {
+
+    public static int SecMaxElemet(int[] Arr){
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
+
+        for(int i=0; i<Arr.length; i++ ){
+            if(Arr[i]>max){
+                secMax = max;
+                max = Arr[i];
+            }
+            else if(Arr[i]>secMax && Arr[i]!=max){
+                secMax = Arr[i];
+            }
+        }
+        return secMax;
+    }
+    public static void main(String[] args) {
+        int[] Arr = {2,3,43,4,55,4,44,55,3,4,5};
+        System.out.println(SecMaxElemet(Arr));
+    }
+}
